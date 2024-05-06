5ec5b26d977d36ba2daced97fb3dd0c3b614d475
nf-714 <nfuad714@gmail.com>
2024-05-06 19:57:38 +0600

    adding important folders

---
+++ src/app.js
@@ -0,0 +1,14 @@
+/* 
+Todos of folders that needed to be created before starting
+1. db
+2. controllers
+3. middlewares
+4. models
+5. routes
+6. utils
+
+Files:
+1. constants.js
+2. index.js
+3. app.js
+*/
